445a17f99e5361ef24aac60e0833925358bb9961
Andrew Santos <asantos@alumni.nd.edu>
2019-07-09 22:18:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,9 +8,13 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
+  source_array
+end
+
+def map_to_double(source_array)
   count=0 
   while count < source_array.length do
-    source_array[count] *= -1
+    source_array[count] *= 2 
     count += 1
   end
   source_array
