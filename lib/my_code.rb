1ea4814d0a709cc17e9d79c4f05ac904b0751463
Andrew Santos <asantos@alumni.nd.edu>
2019-07-09 22:20:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,8 +29,24 @@ def map_to_squared(source_array)
   source_array
 end
 
+def reduce_to_total (source_array)
+  count=0
+  total = 0
+  while count < source_array.length do
+    total += source_array[count]
+    count += 1
+  end
+  source_array
+end
 
-
+def reduce_to_total (source_array, total = 0)
+  count=0
+  while count < source_array.length do
+    total += source_array[count]
+    count += 1
+  end
+  source_array
+end
 
 
 
